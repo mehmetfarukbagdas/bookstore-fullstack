e77b3290d529935475ac1e0a090e75afbd143bb8
Mehmet Faruk Bağdaş <mehmetfarukbagdas@gmail.com>
2026-09-01 14:51:39 +0300

    Updated

--- backend/BagdasKitapDunyasi.API/Migrations/20260901105748_YorumlaraCevapAlaniEklendi.cs
+++ backend/BagdasKitapDunyasi.API/Migrations/20260901105748_YorumlaraCevapAlaniEklendi.cs
@@ -1,22 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
-
-#nullable disable
-
-namespace BagdasKitapDunyası.API.Migrations
-{
-    /// <inheritdoc />
-    public partial class YorumlaraCevapAlaniEklendi : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-
-        }
-
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-
-        }
-    }
+﻿using Microsoft.EntityFrameworkCore.Migrations; 
+ 
+#nullable disable 
+ 
+namespace BagdasKitapDünyası.API.Migrations 
+{ 
+    /// <inheritdoc /> 
+    public partial class YorumlaraCevapAlaniEklendi : Migration 
+    { 
+        /// <inheritdoc /> 
+        protected override void Up(MigrationBuilder migrationBuilder) 
+        { 
+            migrationBuilder.AddColumn<string>(
+                name: "Cevap",
+                table: "Yorumlar",
+                type: "text",
+                nullable: true);
+        } 
+ 
+        /// <inheritdoc /> 
+        protected override void Down(MigrationBuilder migrationBuilder) 
+        { 
+            migrationBuilder.DropColumn(
+                name: "Cevap",
+                table: "Yorumlar");
+        } 
+    } 
 }
